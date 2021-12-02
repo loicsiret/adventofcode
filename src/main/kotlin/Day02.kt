1b828efd9aae5d07b977af157cc188c7c1a59f3e
lsiret <lsiret@nuglif.com>
2021-12-02 10:22:52 -0500

    Day02 part 01

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,9 +1,15 @@
 fun main() {
-    fun part1(input: List<String>) = input.size
+    fun part1(input: List<String>) = input.asSequence()
+        .map { it.split(" ") }
+        .map { Direction::direction.find(it[0])?.let { it1 -> Command(it1, it[1].toInt()) } }
+        .filterNotNull()
+        .fold(Position(0, 0)) { acc, command -> acc move command }
+        .let { it.depthPos * it.horizontalPos }
 
-    fun part2(input: List<String>) = input.size
 
-    // test if implementation meets criteria from the description, like:
+    fun part2(input: List<String>) = 0
+
+// test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 150)
     check(part2(testInput) == 0)
@@ -11,4 +17,27 @@ fun main() {
     val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
+}
+
+data class Position(val depthPos: Int, val horizontalPos: Int) {
+    infix fun move(command: Command): Position {
+        return when (command.direction) {
+            Direction.Forward -> Position(depthPos, horizontalPos + command.unit)
+            else -> Position(
+                depthPos + command.direction.operation * command.unit,
+                horizontalPos
+            )
+        }
+    }
+}
+
+data class Command(val direction: Direction, val unit: Int)
+enum class Direction(val direction: String, val operation: Int) {
+    Forward("forward", 1),
+    Down("down", 1),
+    Up("up", -1),
+}
+
+inline fun <reified T : Enum<T>, V> ((T) -> V).find(value: V): T? {
+    return enumValues<T>().firstOrNull { this(it) == value }
 }
