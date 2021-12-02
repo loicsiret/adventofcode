79d3ac83b0568b4ef4f98534ba1a9b863b90c1a0
lsiret <lsiret@nuglif.com>
2021-12-02 11:06:04 -0500

    Day02 part 02

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -7,25 +7,45 @@ fun main() {
         .let { it.depthPos * it.horizontalPos }
 
 
-    fun part2(input: List<String>) = 0
+    fun part2(input: List<String>) = input.asSequence()
+        .map { it.split(" ") }
+        .map { Direction::direction.find(it[0])?.let { it1 -> Command(it1, it[1].toInt()) } }
+        .filterNotNull()
+        .fold(Position(0, 0)) { acc, command -> acc moveWithAim command }
+        .let { it.depthPos * it.horizontalPos }
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 150)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
 
-data class Position(val depthPos: Int, val horizontalPos: Int) {
+data class Position(val depthPos: Int, val horizontalPos: Int, val aim: Int = 0) {
     infix fun move(command: Command): Position {
         return when (command.direction) {
             Direction.Forward -> Position(depthPos, horizontalPos + command.unit)
             else -> Position(
                 depthPos + command.direction.operation * command.unit,
                 horizontalPos
+            )
+        }
+    }
+
+    infix fun moveWithAim(command: Command): Position {
+        return when (command.direction) {
+            Direction.Forward -> Position(
+                depthPos + command.unit * aim,
+                horizontalPos + command.unit,
+                aim
+            )
+            else -> Position(
+                depthPos,
+                horizontalPos,
+                aim + command.direction.operation * command.unit
             )
         }
     }
