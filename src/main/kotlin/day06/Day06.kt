e438d17650ee35f609c41881330385723411a621
lsiret <lsiret@nuglif.com>
2021-12-08 22:14:25 -0500

    Day06 part02

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -1,10 +1,27 @@
+fun main() {
+    fun part1(input: String, days: Long) = computeBabies(input, days).size
+
+    fun part2(input: String, days: Long) = computeBabiesOptimize(input, days)
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    part1Test()
+    part2Test()
+    check(part1(testInput.first(), 80) == 5934)
+    check(part2(testInput.first(), 256) == 26984457539L)
+
+    val input = readInput("Day06")
+    println(part1(input.first(), 80))
+    println(part2(input.first(), 256))
+}
+
 fun MutableList<Int>.popBabies(): MutableList<Int> {
     return this.map {
         if (it == 0) 6 else it - 1
     }.toMutableList()
 }
 
-fun computeBabies(initialState: String, max: Int): List<Int> {
+fun computeBabies(initialState: String, max: Long): List<Int> {
     var list = initialState.split(",")
         .map { it.toInt() }
         .toMutableList()
@@ -18,6 +35,26 @@ fun computeBabies(initialState: String, max: Int): List<Int> {
     return list
 }
 
+fun computeBabiesOptimize(initialState: String, max: Long): Long {
+    var list: MutableMap<Int, Long> = initialState.split(",")
+        .map { it.toInt() }
+        .groupingBy { it }
+        .eachCountLong()
+        .toMutableMap()
+
+    var paddWithFiller = 0L
+
+    for (i in 1..max) {
+        println("day $i : $list")
+        list = list.mapKeys { it.key - 1 }.toMutableMap()
+        list.merge(8, paddWithFiller, Long::plus)
+        list.merge(6, list.getOrDefault(-1, 0), Long::plus)
+        list.remove(-1)
+        paddWithFiller = list.getOrDefault(0, 0)
+    }
+    return list.values.sum().also { println(it) }
+}
+
 fun part1Test() {
     val initialState = "3,4,3,1,2"
     check(computeBabies(initialState, 1).toString() == "[2, 3, 2, 0, 1]")
@@ -25,18 +62,27 @@ fun part1Test() {
     check(computeBabies(initialState, 4).toString() == "[6, 0, 6, 4, 5, 6, 7, 8, 8]")
 }
 
-fun main() {
-    fun part1(input: String, days: Int) = computeBabies(input, days).size
+fun part2Test() {
+    val initialState = "3,4,3,1,2"
+    check(computeBabiesOptimize(initialState, 1) == 5L)
+    check(computeBabiesOptimize(initialState, 2) == 6L)
+    check(computeBabiesOptimize(initialState, 4) == 9L)
+    check(computeBabiesOptimize(initialState, 5) == 10L)
+    check(computeBabiesOptimize(initialState, 9) == 11L)
+    check(computeBabiesOptimize(initialState, 12) == 17L)
+    check(computeBabiesOptimize(initialState, 18) == 26L)
+}
 
-    fun part2(input: List<String>) = input.size
+fun <T, K> Grouping<T, K>.eachCountLong(): Map<K, Long> =
+// fold(0) { acc, e -> acc + 1 } optimized for boxing
+    foldTo(destination = mutableMapOf(),
+        initialValueSelector = { _, _ -> kotlin.jvm.internal.Ref.LongRef() },
+        operation = { _, acc, _ -> acc.apply { element += 1L } })
+        .mapValuesInPlace { it.value.element }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
-    part1Test()
-    check(part1(testInput.first(), 80) == 5934)
-//    check(part2(testInput) == 0)
-
-    val input = readInput("Day06")
-    println(part1(input.first(), 80))
-//    println(part2(input))
+inline fun <K, V, R> MutableMap<K, V>.mapValuesInPlace(f: (Map.Entry<K, V>) -> R): MutableMap<K, R> {
+    entries.forEach {
+        (it as MutableMap.MutableEntry<K, R>).setValue(f(it))
+    }
+    return (this as MutableMap<K, R>)
 }
